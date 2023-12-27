dcc718d6fb617b61b05e1f6c48a29db4b6288b3f
Pete Peterson <petersonpf@ornl.gov>
2023-12-27 09:11:39 -0500

    Remove python 2 compatibility

--- psrecord.py
+++ psrecord.py
@@ -30,8 +30,6 @@
 #
 ###############################################################################
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import time
 
 
